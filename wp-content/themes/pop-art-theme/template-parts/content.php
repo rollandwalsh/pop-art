8825f5a3946291c27d3eeeeafbe28fdb8464be28
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-18 17:57:49 -0800

    Blog page fix

--- wp-content/themes/pop-art-theme/template-parts/content.php
+++ wp-content/themes/pop-art-theme/template-parts/content.php
@@ -11,7 +11,7 @@
 ?>
 <?php $categories = get_the_category(); ?>
 <?php $hero = get_the_post_thumbnail(); ?>
-<?php $excerpt = get_the_excerpt(); ?>
+<?php $excerpt = substr(get_the_excerpt(), 0, 80); ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
 	<a href="<?php the_permalink(); ?>">
