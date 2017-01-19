a20699097ea77c7345604504fb45e5c5ded91feb
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-18 19:40:28 -0800

    Photo changes

--- wp-content/themes/pop-art-theme/page-templates/bam.php
+++ wp-content/themes/pop-art-theme/page-templates/bam.php
@@ -7,6 +7,8 @@ get_header(); ?>
 <header id="aboutHero" class="page-hero" role="banner" style="background-image: url(<?php echo (wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'full' )[0]); ?>)">
 	<div class="row align-center">
 		<div class="small-12 columns">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bam/bam_logo_white.png" alt="BAM" style="margin-bottom: 1rem">
+			
 			<h1>Bam!&trade; Assets Managed. Assets Mobilized.</h1>
 
 			<h6><span>Introducing BAM!. A powerful, proprietary, content organization and</span> <span>mobilization platform that streamlines your path to digital agility and efficacy.</span></h6>
