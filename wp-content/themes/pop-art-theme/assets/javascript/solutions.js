3b6c51b8b4f0eda048a291af5a990da9cba59a8d
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-23 07:03:03 -0800

    Solutions js

--- wp-content/themes/pop-art-theme/assets/javascript/solutions.js
+++ wp-content/themes/pop-art-theme/assets/javascript/solutions.js
@@ -1,18 +1,37 @@
 // TYPED.JS
 
+const array1 = ['Website navigation', 'Ad ROI', 'App interface', 'Specs volume'];
+const array2 = ['up the wall', 'batty', 'crazy', 'insane'];
+
 $(function() {
 	$('#issueInput1').typed({
-		strings: ['Website navigation', 'Ad ROI', 'App interface', 'Specs volume'],
+		strings: array1,
 		typeSpeed: 0,
 		shuffle: true
 	})
 	$('#issueInput2').typed({
-		strings: ['up the wall', 'batty', 'crazy', 'insane'],
+		strings: array2,
 		typeSpeed: 0,
 		shuffle: true
 	})
 });
 
+// ACCORDION SCROLL
+
 $('.accordion-title').on('click', function(event) {
-   $('html,body').animate({scrollTop: $(event.target).offset().top}, 'slow');
+	setTimeout(function() {
+	   $('html,body').animate({scrollTop: $(event.target).offset().top}, 'slow');
+	}, 250);
+});
+
+// FORM INPUTS
+
+const input1 = document.getElementById('issueInput1');
+input1.addEventListener('focus', function() {
+	if(array1.indexOf(input1.value) > -1) { input1.value = ''; }
+});
+
+const input2 = document.getElementById('issueInput2');
+input2.addEventListener('focus', function() {
+	if(array2.indexOf(input2.value) > -1) { input2.value = ''; }
 });
