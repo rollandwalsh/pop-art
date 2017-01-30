b1f8ba6c8aad413b211d0ea4b9b033842c64da6f
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-30 13:10:05 -0800

    Remove ES2015

--- wp-content/themes/pop-art-theme/assets/javascript/bam.js
+++ wp-content/themes/pop-art-theme/assets/javascript/bam.js
@@ -22,23 +22,23 @@ var bamEndorsementButtons = document.querySelector('.bam-endorsement-buttons');
 
 function bamDamRemoveCurrent() {
 	let bdCurrents = Array.from(document.querySelectorAll('.bam-dam .current'));
-	bdCurrents.forEach(bdCurrent => bdCurrent.classList.remove('current'));
+	bdCurrents.forEach(function(bdCurrent) {bdCurrent.classList.remove('current')});
 }
 
 function bamEndorsementRemoveCurrent() {
 	let beCurrents = Array.from(document.querySelectorAll('.bam-endorsement .current'));
 	console.log(beCurrents);
-	beCurrents.forEach(beCurrent => beCurrent.classList.remove('current'));
+	beCurrents.forEach(function(beCurrent) {beCurrent.classList.remove('current')});
 }
 
 function bamDamAddCurrent(n) {
 	var newCurrents = Array.from(document.querySelectorAll('[data-bddemo="' + n + '"]'));
-	newCurrents.forEach(newCurrent => newCurrent.classList.add('current'));
+	newCurrents.forEach(function(newCurrent) {newCurrent.classList.add('current')});
 }
 
 function bamEndorsementAddCurrent(n) {
 	var newCurrents = Array.from(document.querySelectorAll('[data-bedemo="' + n + '"]'));
-	newCurrents.forEach(newCurrent => newCurrent.classList.add('current'));
+	newCurrents.forEach(functionn(ewCurrent) {newCurrent.classList.add('current')});
 }
 
 function bamDamDemoInit() {
@@ -100,9 +100,9 @@ function bamEndorsementChangeSlide(dir) {
 bamDamDemoStart.addEventListener('click', bamDamDemoInit);
 bamDamDemoPrev.addEventListener('click', function() {bamDamChangeSlide('prev')});
 bamDamDemoNext.addEventListener('click', function() {bamDamChangeSlide('next')});
-bamDamDemoNavs.forEach(bamDamDemoNav => bamDamDemoNav.addEventListener('click', function() {bamDamChangeSlide(bamDamDemoNav.dataset.bddemo)}));
+bamDamDemoNavs.forEach(function(bamDamDemoNav) {bamDamDemoNav.addEventListener('click', function() {bamDamChangeSlide(bamDamDemoNav.dataset.bddemo)})});
 
 bamEndorsementStart.addEventListener('click', bamEndorsementInit);
 bamEndorsementPrev.addEventListener('click', function() {bamEndorsementChangeSlide('prev')});
 bamEndorsementNext.addEventListener('click', function() {bamEndorsementChangeSlide('next')});
-bamEndorsementNavs.forEach(bamEndorsementNav => bamEndorsementNav.addEventListener('click', function() {bamEndorsementChangeSlide(bamEndorsementNav.dataset.bedemo)}));
+bamEndorsementNavs.forEach(function(bamEndorsementNav) {bamEndorsementNav.addEventListener('click', function() {bamEndorsementChangeSlide(bamEndorsementNav.dataset.bedemo)})});
