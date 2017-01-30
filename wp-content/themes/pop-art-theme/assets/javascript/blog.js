7e48eb3d6834957dab9256a95408b8cfe25312f5
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-30 14:47:20 -0800

    Feedback notes

--- wp-content/themes/pop-art-theme/assets/javascript/blog.js
+++ wp-content/themes/pop-art-theme/assets/javascript/blog.js
@@ -2,12 +2,12 @@ var filterList = Array.from(document.querySelectorAll('#blogFilterList a'));
 var posts = Array.from(document.querySelectorAll('.post'));
 
 function changeActive(e) {
-	document.querySelector('#blogFilterList li.active').classList.remove('active');
-	e.target.parentElement.classList.add('active');
+	$('#blogFilterList li.active').removeClass('active');
+	e.target.parent().addClass('active');
 }
 
 function filterBlogs(e) {
-	let selected = e.target.textContent;
+	var selected = e.target.textContent;
 	selected = selected.replace(/\s+/g, '-').toLowerCase();
 	selectedClass = 'category-' + selected;
 	if (selectedClass == 'category-all') {selectedClass = 'post'}
