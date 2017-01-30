88a86eb48f0158d0debb4aaef47196b39cca3f88
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-30 10:24:16 -0800

    Newsletter mailchimp signup

--- wp-content/themes/pop-art-theme/index.php
+++ wp-content/themes/pop-art-theme/index.php
@@ -83,7 +83,30 @@ get_header(); ?>
 		</div>
 		
 		<div class="small-12 medium-6 large-5 columns">
-			<?php echo do_shortcode( '[contact-form-7 id="662" title="Subscribe to our Newsletter"]' ); ?>
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="//popart.us14.list-manage.com/subscribe/post?u=13499979ff8372f1fd7000922&amp;id=f4bc239e8f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+						<div class="mc-field-group">
+							<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email*">
+						</div>
+						<div class="mc-field-group">
+							<input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name">
+						</div>
+						<div class="mc-field-group">
+							<input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name">
+						</div>
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_13499979ff8372f1fd7000922_f4bc239e8f" tabindex="-1" value=""></div>
+						<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					</div>
+				</form>
+			</div>
+			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			<!--End mc_embed_signup-->
 		</div>
 	</div>
 </section>
