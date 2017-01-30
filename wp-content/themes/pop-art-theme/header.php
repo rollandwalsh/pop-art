9e936874450cf32584c238ed784baa30133d5a67
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-30 12:40:36 -0800

    Mobile feedback

--- wp-content/themes/pop-art-theme/header.php
+++ wp-content/themes/pop-art-theme/header.php
@@ -33,7 +33,7 @@
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<div data-sticky-container>
-		<div data-sticky data-sticky-on-small data-options="marginTop:0;" style="width:100%; background-color: #fff; padding: 1rem 0;">
+		<div data-sticky data-options="marginTop:0; stickyOn:small" style="width:100%; background-color: #fff; padding: 1rem 0;">
 			<div class="row">
 				<header id="masthead" class="site-header" role="banner">
 					<div class="title-bar" data-responsive-toggle="site-navigation" data-hide-for="large">
