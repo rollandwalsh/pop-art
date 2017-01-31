5fe2fdff60f0ab6610440707ca38de41e81d8406
Rolland Walsh <rollandwalsh@gmail.com>
2017-01-31 13:40:04 -0800

    Blog page console.log

--- wp-content/themes/pop-art-theme/assets/javascript/blog.js
+++ wp-content/themes/pop-art-theme/assets/javascript/blog.js
@@ -3,7 +3,7 @@ var posts = Array.from(document.querySelectorAll('.post'));
 
 function changeActive(e) {
 	$('#blogFilterList li.active').removeClass('active');
-	console.log(e.target.closest('li'));
+	e.target.closest('li').addClass('active');
 }
 
 function filterBlogs(e) {
